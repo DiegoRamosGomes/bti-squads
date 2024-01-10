21d04bb3cdc469e3469cd88be3ec411ba39de8c3
Diego <diegoramosgomes5@gmail.com>
2024-01-10 17:42:13 -0300

    feat - added GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import './globals.css'
 import { ReactNode } from "react";
+import Script from "next/script";
 
 const inter = Open_Sans({ subsets: ['latin'] })
 
@@ -11,9 +12,23 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const GA_MEASUREMENT_ID = process.env.GA_MEASUREMENT_ID
+  
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <body className={inter.className}>
+    {children}
+    <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}/>
+    <Script id="google-analytics">
+      {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+    </Script>
+    </body>
     </html>
   )
 }
